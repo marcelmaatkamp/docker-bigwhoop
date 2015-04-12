6a29223486779dec8b6c0d7cf944e42c2b29318a
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-04-12 11:25:30 +0200

    cleanup

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --entrypoint bash --privileged -v $PWD/config:/gnuradio/DGSN_bigwhoop/node/config marcelmaatkamp/bigwhoop
+docker run -ti --privileged -v $PWD/config:/gnuradio/DGSN_bigwhoop/node/config marcelmaatkamp/bigwhoop
